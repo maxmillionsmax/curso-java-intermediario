e1a67d368ef7a1c1f28467d2e132125a2f897dca
maxmillionsmax <maximilianocaetano@gmail.com>
2022-07-29 22:41:14 -0300

    Threads: Notify, wait e notifyAll

---
+++ src/com/maximiliano/cursojava/aula72/Teste.java
@@ -0,0 +1,22 @@
+package com.maximiliano.cursojava.aula72;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		TiqueTaque tt = new TiqueTaque();
+		ThreadTiqueTaque tique = new ThreadTiqueTaque("Tique", tt);
+		ThreadTiqueTaque taque = new ThreadTiqueTaque("Taque", tt);
+		
+		try {
+			tique.t.join();
+			taque.t.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
