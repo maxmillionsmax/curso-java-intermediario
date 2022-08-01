1ef3746aa27b1e566504102d7e06c4590365d819
maxmillionsmax <maximilianocaetano@gmail.com>
2022-08-01 00:14:14 -0300

    String: juntando e separando String(join, split

---
+++ src/com/maximiliano/cursojava/aula75_84/Aula82.java
@@ -0,0 +1,25 @@
+package com.maximiliano.cursojava.aula75_84;
+
+public class Aula82 {
+
+	public static void main(String[] args) {
+
+
+		String alfabeto = String.join(", ", "A","B","C","D");
+		System.out.println(alfabeto);
+		
+		String[] letras = alfabeto.split(", ");
+		
+		for (String letra : letras) {
+			System.out.print(letra);
+		}
+		System.out.println();
+		
+		String doArquivo = "1;Antonio;30";
+		String[] infos = doArquivo.split(";");
+		Pessoa pessoa = new Pessoa(Integer.parseInt(infos[0]), infos[1], Integer.parseInt(infos[2]));
+		System.out.println(pessoa);
+		
+	}
+
+}
