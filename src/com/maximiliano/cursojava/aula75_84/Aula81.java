e6f8a0565b1d17b78523f8ec2038d2ffd42484fa
maxmillionsmax <maximilianocaetano@gmail.com>
2022-07-31 23:14:28 -0300

    String: modificando letras maiúsculas/minusculas(toLowerCase, toUpperCase

---
+++ src/com/maximiliano/cursojava/aula75_84/Aula81.java
@@ -0,0 +1,21 @@
+package com.maximiliano.cursojava.aula75_84;
+
+public class Aula81 {
+
+	public static void main(String[] args) {
+
+
+		String teste = "Teste.";
+		String testeMinusculo = teste.toLowerCase();
+		String testeMaisculo = teste.toUpperCase();
+		
+		System.out.println(testeMaisculo);
+		System.out.println(testeMinusculo);
+		
+		if (teste.toLowerCase().equals(teste.toLowerCase())) {
+			
+		}
+		
+	}
+
+}
